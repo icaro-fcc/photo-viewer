0f7b37ccb1a5d313e372a5dc6c4aaabd59683d1d
Icaro Ferreira <icrfcc@gmail.com>
2023-07-31 13:50:43 -0300

    Organize the functions on script.js
    There are more comments and better separation of the functions, as well as some changes in both photos.js and script.js for a better randomNum generation without errors

--- assets/js/photos.js
+++ assets/js/photos.js
@@ -1,6 +1,7 @@
 export let photosList = [];
+export let totalPhotos = 20;  // <-- Define here how many photos exist in the source folder!
 
-for (let i = 0; i <= 20; i++) {
+for (let i = 0; i <= totalPhotos; i++) {
     photosList.push({
         source: `./assets/img/photos/photo (${i}).jpg`
     });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,133 @@
-import {photosList} from '../js/photos.js';
+import {photosList, totalPhotos} from './photos.js';
 
 $(document).ready(function() {
 
 
-    for (let i = 1; i <= 20; i++) {
-        let randomNum = Math.floor(Math.random() * 20) + 1;
-        console.log(randomNum); // outputs a random integer between 1 and 1607
-        let newPhotoElement = $('<img>');
-        newPhotoElement.attr('src', photosList[randomNum].source);
-        newPhotoElement.addClass('photo');
-        console.log(newPhotoElement);
-        let newPhotoWrapperElement = $('<div>');
-        newPhotoWrapperElement.addClass('photoWrapper');
-        newPhotoWrapperElement.append(newPhotoElement);
-        $('.slide').append(newPhotoWrapperElement);
+    let photosToShow = 20;    // <-- Define here how many photos will be shown in the slider!
 
-        //Setting the 1st random photo as the 1st one showing on the page
-        if (i == 1)
-        $('.photoDisplay').css('background-image', `url('${photosList[randomNum].source}'`);
-    }
+    /*Setting 20 photos to show and there are 20 photos in the folder,
+    so there will be repeated ones. This is just for adding many to the slider*/
+
+    /*This function selects the photos randomly to be used in the slider,
+    wrapping and adding their respective wrapper classes*/
+    function photosSetUp() {       
+
+        photosToShow = (photosToShow > totalPhotos) ? totalPhotos : photosToShow;
+
+        for (let i = 1; i <= photosToShow; i++) {
+            let randomNum = Math.floor(Math.random() * totalPhotos);
+
+            let newPhotoElement = $('<img>');
+            console.log('Random number: ', randomNum);
+            newPhotoElement.attr('src', photosList[randomNum].source);
+            newPhotoElement.addClass('photo');
+
+            let newPhotoWrapperElement = $('<div>');
+            newPhotoWrapperElement.addClass('photoWrapper');
+            newPhotoWrapperElement.append(newPhotoElement);
+
+            $('.slide').append(newPhotoWrapperElement);
+
+            //Setting the 1st random photo as the 1st one showing on the page
+            if (i == 1)
+                $('.photoDisplay').css('background-image', `url('${photosList[randomNum].source}'`);
+        };
+
+        eventsSetUp(); //After creating the photos elements, creating the events for each
+    };
 
     
 
-    console.log(photosList[0]);
-
-    //Applying the hover function for all the photo elements
-    // $('.photoWrapper').hover(function() {
-    //     console.log('Hovering...');
-    //     $(this).find('.photo').css('transform', 'scale(1.3)');
-    // }, function(){
-    //     $(this).find('.photo').css('transform', 'scale(1)');
-    // });
-
-    $('.photo').hover(function() {
-        console.log('Hovering...');
-        $(this).parent().css('transform', 'scale(1.3)');
-    }, function(){
-        $(this).parent().css('transform', 'scale(1)');
-    });
-
-
-    let currentPhotoElement;
-
-    $('.photo').bind('click', function() {
-        $('.photoDisplay').css('background-image', `url('${$(this).attr('src')}'`);
-        currentPhotoElement = $(this);
-        console.log(currentPhotoElement);
-        
-
-        $('.photo').each(function() {
-            if ($(this) != currentPhotoElement) {
-                console.log('Setting the border of the element: ', $(this));
-                $(this).css('border', 'none');
-            }
-                
+    /*This function is responsible for adding the events for photos and arrow buttons,
+    as well as displaying the photo that's clicked*/
+    function eventsSetUp() {
+        $('.photo').hover(function() {
+            $(this).parent().css('transform', 'scale(1.3)');
+        }, function(){
+            $(this).parent().css('transform', 'scale(1)');
+        });
+    
+    
+        let currentPhotoElement;
+    
+        $('.photo').bind('click', function() {
+            $('.photoDisplay').css('background-image', `url('${$(this).attr('src')}'`);
+            currentPhotoElement = $(this);
+            
+    
+            $('.photo').each(function() {
+                if ($(this) != currentPhotoElement)
+                    $(this).css('border', 'none');                    
+            });
+    
+            currentPhotoElement.css('border', '5px solid #CCC');
         });
 
-        $(this).css('border', '5px solid #CCC');
-    });
+    };
 
 
 
-    // $('.arrowRightEnd').bind('click', function() {
-    //     if ($(currentPhotoElement).next()) {
-    //         console.log('Foi pra foto da direita');
-    //         console.log(currentPhotoElement);
-    //         console.log(currentPhotoElement.next().attr('src'));
-    //         $('.photoDisplay').css('background-image', `url('${$(currentPhotoElement).next().attr('src')}'`);
-    //     }
-            
-    // });
+    /*This function gets the sizes of the elements involved in the slider such as total width's
+    and sends them to the function that will handle the navigation according to them*/
+    function setSliderSizes() {
+        //The total width (the wrapper's width) of a single photo
+        let photoWrapperWidth = parseInt($('.photoWrapper').eq(0).css('width'), 10);
+        let photoWrapperMargin = (parseInt($('.photoWrapper').eq(0).css('margin-left'), 10)) * 2;
+        let photoTotalWidth = photoWrapperWidth + photoWrapperMargin;
 
-    $('.arrows').hover(function() {
-        $('.arrowLeftEnd, .arrowRightEnd').css('opacity', 1);
-    }, function() {
-        $('.arrowLeftEnd, .arrowRightEnd').css('opacity', '1');
-    });
+        let slideMargin = 0;
+        let slideWidth = ($('.photoWrapper').length * photoTotalWidth);
+        //the total width of all photos
+
+        let galleryWidth = parseInt($('.gallery').css('width'), 10);
+        //the width of the gallery (slide) which is based on the viewport
+
+        console.log('slideWidth: ', slideWidth, 'e galleryWidth: ', galleryWidth);
+
+        
+        let maxRightClicks = Math.ceil(photosToShow - (galleryWidth / photoTotalWidth));
+        //Number of clicks that will be possible to go to the right
+        /*
+        This math was thought like this: since the galleryWidth (which is based on viewport)
+        divided by photoTotalWidth (each photo) is the number of photos that are firstly shown on the screen,
+        then if considering the total of photos, and subtracting this number of photos shown,
+        we know how many clicks (which takes a single photo, or photoTotalWidth) will take until
+        reaching the last photo on the slider. It works for any viewport size.
+        */
+
+        /*Checking if the slide width (based on the number of photos to show) is shorter
+        than the screen width. If so, then it'll be adjusted better on the screen*/
+        if (slideWidth < galleryWidth) {
+            $('.slide').css('display', 'flex').css('justify-content', 'center');
+            $('.gallery').css('display', 'flex').css('justify-content', 'center');
+            $('.arrows').css('display', 'none');
+
+        } else {
+            //Calling the function that will handle the slider navigation
+            handleSliderArrowClick(photoTotalWidth, maxRightClicks, slideMargin);
+
+        }
+    };
 
 
 
-    let slideMargin = 0;
-    let slideWidth = ($('.photoWrapper').length * 90);
-    let galleryWidth = parseInt($('.gallery').css('width'), 10);
-    console.log('slideWidth: ', slideWidth, 'e gallery.length: ', galleryWidth);
+    /*This function handles the click events for the arrows (navigation), as well as
+    the limits for showing the beginning and end of the slide*/
+    function handleSliderArrowClick(photoTotalWidth, maxRightClicks, slideMargin) {
+        let slideMarginLimit = - (photoTotalWidth * maxRightClicks);
 
-    if (slideWidth < galleryWidth) {
-        $('.slide').css('display', 'flex').css('justify-content', 'center');
-    } else {
-        let slideLengthLimit = -($('.photoWrapper').length - 16) * 90;
-
-        console.log('slideLengthLimit: ', slideLengthLimit);
+        console.log('maxRightClicks: ', maxRightClicks);
+        console.log('slideMarginLimit: ', slideMarginLimit);
 
         
         console.log($('.photoWrapper').length);
-    
-        
-    
-        //60px is the length of every photo element
+
     
         $('.arrowRight').bind('click', function() {
             
-            if (slideMargin >= slideLengthLimit) {
+            if (slideMargin > slideMarginLimit) {
                 console.log(slideMargin);
-                    slideMargin -= 90;
+                    slideMargin -= photoTotalWidth;
                 $('.slide').css('margin-left', slideMargin);
                 console.log(slideMargin);
             }
@@ -113,14 +138,18 @@ $(document).ready(function() {
             
             console.log(slideMargin);
             if (slideMargin < 0) {
-                slideMargin += 90;
+                slideMargin += photoTotalWidth;
                 $('.slide').css('margin-left', slideMargin);
                 console.log(slideMargin);
             }
             
         });
-    }
+    };
 
+
+    //Initializing the slider:
+    photosSetUp();
+    setSliderSizes();
 
     
 });
